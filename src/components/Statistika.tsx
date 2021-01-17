0a634aefd67118d73e4bd8c4c8c7a27dd589b730
tea243 <tea.korac@gmail.com>
2021-01-17 23:57:21 +0100

    Dodata nova statistika: broj pogodaka

--- src/components/Statistika.tsx
+++ src/components/Statistika.tsx
@@ -8,13 +8,18 @@ export default function Statistika(props: Props) {
     const duzina = props.pokusaji.length;
     const brojGlava = props.pokusaji.filter(element => element.glava).length;
     const brojPismo = duzina - brojGlava;
+    const brojPogodaka = props.pokusaji.filter(element => element.pogodio).length;
     return (
         <div className='container mt-5'>
             <StavkaStatistike naziv='Ukupno pokusaja' vrednost={duzina} />
+            <StavkaStatistike naziv='Broj pogodaka' vrednost={brojPogodaka} />
+            {duzina > 0 && <StavkaStatistike naziv='Procenat pogodaka' vrednost={brojPogodaka / duzina} />}
             <StavkaStatistike naziv='Ukupno glava' vrednost={brojGlava} />
             {duzina > 0 && <StavkaStatistike naziv='Procentualno glava' vrednost={brojGlava / duzina} />}
             <StavkaStatistike naziv='Ukupno pismo' vrednost={brojPismo} />
             {duzina > 0 && <StavkaStatistike naziv='Procentualno pismo' vrednost={brojPismo / duzina} />}
+            <StavkaStatistike naziv='Ukupno pismo' vrednost={brojPismo} />
+
         </div>
     )
 }
